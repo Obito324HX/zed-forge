a1628271f83c8517937d22b8630a55570596a6db
Obito324HX <Obito324HX@users.noreply.github.com>
2026-08-14 03:37:25 +0200

    Remove Insights and Technical Skills sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import { Footer } from "@/components/layout/Footer";
 import { Navbar } from "@/components/layout/Navbar";
 import { About } from "@/components/home/About";
-import { Blog } from "@/components/home/Blog";
 import { Contact } from "@/components/home/Contact";
 import { FeaturedWork } from "@/components/home/FeaturedWork";
 import { Hero } from "@/components/home/Hero";
 import { Process } from "@/components/home/Process";
 import { Services } from "@/components/home/Services";
-import { Skills } from "@/components/home/Skills";
 import { Team } from "@/components/home/Team";
 
 export default function Home() {
@@ -19,10 +17,8 @@ export default function Home() {
         <FeaturedWork />
         <Services />
         <Process />
-        <Skills />
         <About />
         <Team />
-        <Blog />
         <Contact />
       </main>
       <Footer />
